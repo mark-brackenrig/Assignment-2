0d8cac9d2fc029873e411f11cd64e676ba48cac7
martin-lehmann <martin@madlytics.com>
2018-04-29 00:20:10 +1000

    tidy up tourism acquire

--- Preprocessing/tourism_acquire.r
+++ Preprocessing/tourism_acquire.r
@@ -48,45 +48,52 @@ data_endRow = 17
 data_endCol = 12
 data_nextRowStep = 12
 
+# Helper functions
+# Get specific Cell Value from sheet
+getCellValue <- function(workbook, sheet, row, col) {
+  names(readWorksheet(workbook, sheet, startRow = row, startCol = col, endRow = row, endCol = col))
+}
+
+# Get data frame for bounded box
+getDataTable <- function(workbook, sheet, start_row, start_col, end_row, end_col) {
+  readWorksheet(workbook, sheet, startRow = start_row, startCol = start_col, endRow = end_row, endCol = end_col)
+}
+
+# Visit dimension name cleaned for friendly format i.e. friendly_feature_name
+cleanVisitDimensionName <- function(s) {
+  s %>% 
+    str_replace_all("\\.", " ") %>%   # Replace all . with spaces
+    str_replace_all("  ", " ") %>%    # Collapse double spaces
+    str_extract("[A-Z ]+") %>%        # Get words only
+    trimws() %>%                      # Remove leading and trailing whitespace
+    str_replace_all(" ", "_") %>%     # Replace spaces with underscore
+    tolower()                         # Lower case
+}
+
+# Final Tourism data frame
 tourism.data = data.frame()
 
 # Build dataset across all sheets
 for (sheet in sheet_names){
 
-  # empty data sheet
-  sheet_data <- data.frame(purpose=character(),
-                           year=integer(), 
-                           stringsAsFactors=FALSE) 
+  # Context data for current sheet
+  sheet_data <- data.frame() 
   
   # Loop through each section in the sheet as they are iterated over each visit dimension
   for (i in sheet_section_range) {
 
-    # visit dimension name, what in format lower case words seperated by underscore
-    # get cell
-    visit_dimension_name = names(readWorksheet(tourism_workbook, 
-                                         sheet, 
-                                         startRow = visitor_dimension_startRow + ((i-1) * visitor_dimension_nextRowStep), 
-                                         startCol = visitor_dimension_col,
-                                         endRow = visitor_dimension_startRow + ((i-1) * visitor_dimension_nextRowStep), 
-                                         endCol = visitor_dimension_col
-    ))
+    # visit dimension name, want in format lower case words seperated by underscore
+    visit_dimension_name = getCellValue(tourism_workbook, sheet, 
+                                        visitor_dimension_startRow + ((i-1) * visitor_dimension_nextRowStep), 
+                                        visitor_dimension_col)
     
-    # Make friendly feature name 
-    visit_dimension_name %>% 
-      str_replace_all("\\.", " ") %>%   # Replace all . with spaces
-      str_extract("[A-Z ]+") %>%        # Get words only
-      trimws() %>%                      # Remove leading and trailing whitespace
-      str_replace_all(" ", "_") %>%     # Replace spaces with underscore
-      tolower() -> visit_dimension_name # Lower case
+    # Visit Dimension Metric
+    visit_dimension_name <- cleanVisitDimensionName(visit_dimension_name)
     
-    # get sheet data for visit dimension
-    sheet_section_data <- readWorksheet(tourism_workbook, 
-                                sheet, 
-                                startRow = data_startRow + ((i-1) * data_nextRowStep), 
-                                startCol = data_startCol,
-                                endRow = data_endRow + ((i-1) * data_nextRowStep), 
-                                endCol = data_endCol
-    )
+    # get sheet data for visit dimension Metric
+    sheet_section_data <- getDataTable(tourism_workbook, sheet,
+                                       data_startRow + ((i-1) * data_nextRowStep), data_startCol,
+                                       data_endRow + ((i-1) * data_nextRowStep), data_endCol)
 
     # Change names of features to "purpose" and from 2007, to 2017
     names(sheet_section_data) <- c("purpose", sheet_year_range)
@@ -94,9 +101,8 @@ for (sheet in sheet_names){
     # Strip leading and trailing whitespace
     sheet_section_data$purpose <- trimws(sheet_section_data$purpose)
     
-    # Strip total row
+    # Strip total row, and backpacker data
     sheet_section_data <- sheet_section_data[!(sheet_section_data$purpose == "Total"),]
-    # Strip backpackers
     sheet_section_data <- sheet_section_data[!(sheet_section_data$purpose == "Backpackers"),]
     sheet_section_data <- sheet_section_data[!(sheet_section_data$purpose == "Non backpackers"),]
     
@@ -116,28 +122,20 @@ for (sheet in sheet_names){
   
   # add country name, this is the current sheet name
   sheet_data %>% 
-    mutate(country = sheet) -> sheet_data
+    mutate(country = as.factor(sheet)) -> sheet_data
   
   # need to append now this dataset
-  if (nrow(tourism.data) == 0) {
-    tourism.data <- sheet_data
-  } else {
-    # coerce types into original dataset
-    
-    # if sheet section data is not numeric, convert to numeric
-    # where np replace with NA
-    #cleanNumeric <- function(s) {
-    #  as.numeric(destring(s))
-    #}
-    
-    #sheet_section_data <-
-    #  cbind(
-    #    sheet_section_data[,1],
-    #    apply(sheet_section_data[,c(3:data_endCol-data_startCol)], 2, cleanNumeric)
-    #  )
-    
-    tourism.data <- rbind(tourism.data, sheet_data)
-  }
+  tourism.data <- rbind(tourism.data, sheet_data)
 }
 
+# Clean metric data, and put as proper type
+dim_index <- which(names(tourism.data) %in% c("purpose", "year","country"))
+metric_columns <- names(tourism.data[-dim_index])
 
+for (col in metric_columns) {
+  tourism.data[,col] <- as.numeric(destring(tourism.data[,col])) # convert to number, and set np to NA
+}
+
+# Check fine
+str(tourism.data)
+head(tourism.data)
