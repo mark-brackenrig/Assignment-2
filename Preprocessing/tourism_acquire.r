8c1b2059ea4d2bc49001d125b5a705e0bba9c99b
martin-lehmann <martin@madlytics.com>
2018-04-29 01:10:42 +1000

    clean up tourism acquire

--- Preprocessing/tourism_acquire.r
+++ Preprocessing/tourism_acquire.r
@@ -10,14 +10,14 @@ if(!"reshape2" %in% rownames(installed.packages())) {
 }
 
 # Use XLConnect to deal with multiple worksheets
-library(XLConnect)
-library(reshape2)
-library(stringr)
-library(taRifx)   # work with NA when destring
+library(XLConnect)  # excel
+library(reshape2)   # melt
+library(stringr)    # regex
+library(taRifx)     # destring
 
-base_path <- "G:/Team Drives/STDS - Assignment 2 - 3MDL/Dataset/Accommodation/"
+tourism_base_path <- "G:/Team Drives/STDS - Assignment 2 - 3MDL/Dataset/Accommodation/"
 tourism_filename <- "IVS1 YE Dec 2017_UpdatedMar2018.xlsx"
-tourism_fullpath <- paste(base_path, tourism_filename, sep="")
+tourism_fullpath <- paste(tourism_base_path, tourism_filename, sep="")
 
 # Load tourism workbook
 tourism_workbook <- loadWorkbook(tourism_fullpath)
@@ -25,23 +25,20 @@ tourism_workbook <- loadWorkbook(tourism_fullpath)
 # Get sheet names
 sheet_names <- getSheets(tourism_workbook)
 
-# inspect sheet names
-sheet_names
-
 # We are only interested in country sheets 
 sheet_names <- sheet_names[!sheet_names %in% c("Contents", "Total")]
-
 
 # Spreadsheet configuration details
 
 # There are 7 visit dimensions located in Cell A8 and increasing by 12 rows with last dimensions in Cell A80
-sheet_section_range = 1:7
+sheet_block_range = 1:7
 sheet_year_range = 2007:2017
-visitor_dimension_col = 1
+
 visitor_dimension_startRow = 8
+visitor_dimension_col = 1
 visitor_dimension_nextRowStep = 12
 
-# Each visit dimension has its own associated section of data
+# Each visit dimension has its own associated block of data
 data_startRow = 8
 data_startCol = 1
 data_endRow = 17
@@ -49,17 +46,9 @@ data_endCol = 12
 data_nextRowStep = 12
 
 # Helper functions
-# Get specific Cell Value from sheet
-getCellValue <- function(workbook, sheet, row, col) {
-  names(readWorksheet(workbook, sheet, startRow = row, startCol = col, endRow = row, endCol = col))
-}
-
-# Get data frame for bounded box
-getDataTable <- function(workbook, sheet, start_row, start_col, end_row, end_col) {
-  readWorksheet(workbook, sheet, startRow = start_row, startCol = start_col, endRow = end_row, endCol = end_col)
-}
 
 # Visit dimension name cleaned for friendly format i.e. friendly_feature_name
+# features are currently of format UPPER.WORDS.AND.SO..ON.(000)
 cleanVisitDimensionName <- function(s) {
   s %>% 
     str_replace_all("\\.", " ") %>%   # Replace all . with spaces
@@ -68,6 +57,23 @@ cleanVisitDimensionName <- function(s) {
     trimws() %>%                      # Remove leading and trailing whitespace
     str_replace_all(" ", "_") %>%     # Replace spaces with underscore
     tolower()                         # Lower case
+}
+
+# Get Visit Dimension Name from sheet
+getVisitDimensionName <- function(workbook, sheet, block_index) {
+  visit_dimension_name = readWorksheet(workbook, sheet, 
+                                       startRow = visitor_dimension_startRow + ((block_index-1) * visitor_dimension_nextRowStep), 
+                                       startCol = visitor_dimension_col, 
+                                       endRow = visitor_dimension_startRow + ((block_index-1) * visitor_dimension_nextRowStep), 
+                                       endCol = visitor_dimension_col)
+  cleanVisitDimensionName(names(visit_dimension_name))
+}
+
+# Get Visit Dimension Block Data from sheet
+getVisitDimensionBlock <- function(workbook, sheet, block_index) {
+  readWorksheet(workbook, sheet, 
+                startRow = data_startRow + ((block_index-1) * data_nextRowStep), startCol = data_startCol, 
+                endRow = data_endRow + ((block_index-1) * data_nextRowStep), endCol = data_endCol)
 }
 
 # Final Tourism data frame
@@ -79,44 +85,29 @@ for (sheet in sheet_names){
   # Context data for current sheet
   sheet_data <- data.frame() 
   
-  # Loop through each section in the sheet as they are iterated over each visit dimension
-  for (i in sheet_section_range) {
+  # Loop through each block in the sheet as they are iterated over each visit dimension
+  for (block_index in sheet_block_range) {
 
-    # visit dimension name, want in format lower case words seperated by underscore
-    visit_dimension_name = getCellValue(tourism_workbook, sheet, 
-                                        visitor_dimension_startRow + ((i-1) * visitor_dimension_nextRowStep), 
-                                        visitor_dimension_col)
-    
-    # Visit Dimension Metric
-    visit_dimension_name <- cleanVisitDimensionName(visit_dimension_name)
-    
     # get sheet data for visit dimension Metric
-    sheet_section_data <- getDataTable(tourism_workbook, sheet,
-                                       data_startRow + ((i-1) * data_nextRowStep), data_startCol,
-                                       data_endRow + ((i-1) * data_nextRowStep), data_endCol)
+    sheet_block_data <- getVisitDimensionBlock(tourism_workbook, sheet, block_index)
 
     # Change names of features to "purpose" and from 2007, to 2017
-    names(sheet_section_data) <- c("purpose", sheet_year_range)
+    names(sheet_block_data) <- c("purpose", sheet_year_range)
     
-    # Strip leading and trailing whitespace
-    sheet_section_data$purpose <- trimws(sheet_section_data$purpose)
-    
-    # Strip total row, and backpacker data
-    sheet_section_data <- sheet_section_data[!(sheet_section_data$purpose == "Total"),]
-    sheet_section_data <- sheet_section_data[!(sheet_section_data$purpose == "Backpackers"),]
-    sheet_section_data <- sheet_section_data[!(sheet_section_data$purpose == "Non backpackers"),]
+    # visit dimension name, want in format lower case words seperated by underscore
+    visit_dimension_name = getVisitDimensionName(tourism_workbook, sheet, block_index)
     
     # Melt the years features into a single year feature
-    sheet_section_data %>% 
+    sheet_block_data %>% 
       melt(id.vars=c("purpose"),
            variable.name="year",
-           value.name=visit_dimension_name) -> sheet_section_data
+           value.name=visit_dimension_name) -> sheet_block_data
    
-    # need to join
+    # Each block is a feature, inner join on purpose and year
     if (nrow(sheet_data) == 0) {
-      sheet_data <- sheet_section_data # first time
+      sheet_data <- sheet_block_data # first time
     } else {
-      sheet_data <- merge(sheet_data, sheet_section_data, by=c("purpose","year"))
+      sheet_data <- merge(sheet_data, sheet_block_data, by=c("purpose","year"))
     }
   }
   
@@ -127,6 +118,19 @@ for (sheet in sheet_names){
   # need to append now this dataset
   tourism.data <- rbind(tourism.data, sheet_data)
 }
+
+# Clean Tourism.data
+
+# Strip leading and trailing whitespace
+tourism.data$purpose <- trimws(tourism.data$purpose)
+
+# Strip total row, and backpacker data
+tourism.data <- tourism.data[!(tourism.data$purpose == "Total"),]
+tourism.data <- tourism.data[!(tourism.data$purpose == "Backpackers"),]
+tourism.data <- tourism.data[!(tourism.data$purpose == "Non backpackers"),]
+
+# Convert to factor
+tourism.data$purpose <- as.factor(tourism.data$purpose)
 
 # Clean metric data, and put as proper type
 dim_index <- which(names(tourism.data) %in% c("purpose", "year","country"))
@@ -139,3 +143,5 @@ for (col in metric_columns) {
 # Check fine
 str(tourism.data)
 head(tourism.data)
+
+# Merge with AirBNB
