c01b89728e0950bfc3c3f0853cc548fb65ad34a0
mark-CoreData <mark.brackenrig@coredata.com.au>
2018-04-28 19:35:49 +1000

    Changes to the index file and finalised the cleaning script for the listings file. Things to note: 1. I have removed a small amount of records as they had inconsistencies (such as saying they were in another state or market) 2. I have removed all fields that didnt have any variation or that were superfluous 3. Character variables that should be factors were converted. Some were not, but that was because they may need some additional cleaning (I felt these fields were less important so I didnt bother) 4. I have created two additional tables - Amenities and Host verification                         - These are an expansion of the amenities and host verfication fields that had numerous subfields (they kinda looked like JSON)                        - I left them out for simplicity

--- Preprocessing/Cleaning.R
+++ Preprocessing/Cleaning.R
@@ -8,6 +8,29 @@
 # remove relevant special characters in text fields
 # 
 
+###NOTES####
+# Inspect Market and State fields.... Clearly some scraping issues
+# I suggest that we just remove them
+
+## Remove unneeded ID fields, and fields with no information
+
+#No information in these fields (i.e. all the same response), lets remove it
+
+listings$experiences_offered <- NULL #no information
+listings$host_acceptance_rate <- NULL #no information
+listings$scrape_id <- NULL #Doesnt link to anything ie. redundant field
+listings$host_name <- NULL #We have host ID... lets remove for privacy reasons
+listings$country <- NULL #We are only looking at Sydney
+listings$country_code <- NULL #as above
+listings$has_availability <- NULL #No information
+listings$requires_license <- NULL #no information
+listings$license <- NULL #No information
+#Remove incosistent fields...
+listings <- subset(listings, !listings$state %in% c("VIC", "Queensland")&!listings$market %in% c("Gold Coast-Tweed","Central Florida Atlantic Coast","Prague")&is.na(listings$jurisdiction_names))
+listings$jurisdiction_names <- NULL
+
+
+
 ## Price Fields 
     #clean Price fields
     priceclean <- function(x){
@@ -17,7 +40,7 @@
     }
     
     #Check price fields
-    subset(colnames(listings),grepl("price",tolower(colnames(listings)))==T)
+    #subset(colnames(listings),grepl("price",tolower(colnames(listings)))==T)
     
     #other price fields clean
     listings$price <- priceclean(listings$price)
@@ -29,16 +52,6 @@
     listings$cleaning_fee <- priceclean(listings$cleaning_fee)
     listings$extra_people <- priceclean(listings$extra_people)
     
-    
-## Experience
-    unique(listings$experiences_offered)
-    #No information in this field, lets remove it
-    
-listings$experiences_offered <- NULL
-listings$host_acceptance_rate <- NULL
-
-
-
 
 ## Remove URLs
 listings <- subset(listings, select = c( subset(colnames(listings),grepl("url",tolower(colnames(listings)))==F)))
@@ -47,21 +60,33 @@ listings <- subset(listings, select = c( subset(colnames(listings),grepl("url",t
 #Host response rate
 listings$host_response_rate <- gsub(pattern = "\\%", replacement = "", listings$host_response_rate)
 
-#this throws a warning, but its okay, it just coerces "N/A" to NA
+#Coerce the text null values to actual null values
+#this is cosmetic code, its just so that R doesnt throw a warning
+listings$host_response_rate[listings$host_response_rate=="N/A"] <- NA
+
+#Convert to numeric
 listings$host_response_rate <- as.numeric(listings$host_response_rate)
 
-
-
 ## Convert t/f to factors ##
-colnames(listings[,grep(pattern = "is_", colnames(listings))])
+#colnames(listings[,grep(pattern = "is_", colnames(listings))])
 
 #columns with "is_" in them tend are t/f
 
-listings$property_type <- as.factor(listings$property_type)
+
 listings$host_is_superhost <- as.factor(listings$host_is_superhost) 
 listings$is_location_exact <- as.factor(listings$is_location_exact) 
-
-
+listings$is_business_travel_ready <-as.factor(listings$is_business_travel_ready)
+listings$instant_bookable <- as.factor(listings$instant_bookable)
+listings$property_type <- as.factor(listings$property_type)
+listings$host_has_profile_pic <- as.factor(listings$host_has_profile_pic)
+listings$host_response_time[listings$host_response_time=="N/A"] <- NA 
+listings$host_response_time <- factor(listings$host_response_time,levels = c("within an hour", "within a few hours", "within a day", "a few days or more"), ordered = T)
+listings$host_identity_verified <- as.factor(listings$host_identity_verified)
+listings$room_type <- as.factor(listings$room_type)
+listings$bed_type <- as.factor(listings$bed_type)
+listings$cancellation_policy <- factor(listings$cancellation_policy,levels = c("flexible", "moderate","strict","super_strict_30"), ordered = T)
+listings$require_guest_phone_verification <- as.factor(listings$require_guest_phone_verification)
+listings$require_guest_profile_picture <- as.factor(listings$require_guest_profile_picture)
 
 #Host Verifications
 Host_Verifications <- subset(listings, select = c("id", "host_verifications"))
@@ -79,9 +104,10 @@ for(i in 1:length(split)){
   colnames(Host_Verifications)[colnames(Host_Verifications)=="temp"] <- paste0("host_verification_",split[i])
 temp <- NULL
 }
+#Remove unneccessary field
 Host_Verifications$host_verifications <- NULL
-
-listings <- merge(Host_Verifications, listings, by  = "id" )
+listings$host_verifications <- NULL
+#listings <- merge(Host_Verifications, listings, by  = "id" )
 
 ## Amenities
 
@@ -102,7 +128,6 @@ for(i in 1:length(split)){
 }
 Amenities$amenities <- NULL
 
-summary(Amenities)
 keep <- c()
 for(i in 2:ncol(Amenities)){
   #Choose 650, i.e. at least approximately 2% of properties should have this feature  
@@ -112,9 +137,16 @@ keep <- c(keep, i)
   }
 }
 
-Amenities <- Amenities[,c(1,keep)]
+#this vector is an index vector of all of the columns I suggest we include in the analysis for amenities
+keep <- c(1,keep)
 
-listings <- merge(Amenities, listings, by  = "id" )
+#You may wish to uncomment out this code. 
+#Amenities <- Amenities[,c(1,keep)]
+
+#Merge into listings - I have removed this because I think it will keep the files a little cleaner at the moment
+#listings <- merge(Amenities, listings, by  = "id" )
+listings$amenities <- NULL
+
 
 
 
--- index.R
+++ index.R
@@ -2,9 +2,11 @@
 
 setwd("C:/Users/mbrackenrig/Documents/Assignment-2")
 
+#Pull in data
 source("Preprocessing/ReadAirbnb.r")
 
 setwd("C:/Users/mbrackenrig/Documents/Assignment-2")
 
+#Clean data - Put it in correct formate, etc..
 source("Preprocessing/Cleaning.r")
 
